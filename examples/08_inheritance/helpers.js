be1992a87f5808cc9674dc36d594002ecff98ebf
m-j <jamiolkowski.mateusz@gmail.com>
2013-08-06 18:27:37 +0100

    Helpers

--- examples/08_inheritance/helpers.js
+++ examples/08_inheritance/helpers.js
@@ -8,28 +8,4 @@ function flattenObject(obj){
     return flattenedObject;
 }
 
-var ROOT_PROTO = {}.__proto__;
-
-function deeplySearchForProperty(obj, propertyName, accumulatorArray){
-    accumulatorArray.push(obj[propertyName]);
-
-    if(!obj.__proto__){
-        return accumulatorArray;
-    }
-    else {
-        deeplySearchForProperty(obj.__proto__, propertyName, accumulatorArray);
-    }
-}
-
-function deepFlatten(obj){
-    var flattenedObject = {};
-
-    for(key in obj){
-        flattenedObject[key] = deeplySearchForProperty(obj, key, []);
-    }
-
-    return flattenedObject;
-}
-
 exports.flattenObject = flattenObject;
-exports.deepFlatten = deepFlatten;
